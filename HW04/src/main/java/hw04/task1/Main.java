77660c7750ec73ec5726f35480bb1ab7b35157f5
YevhenKovalevskyi <carsar.sd@gmail.com>
2022-11-08 22:46:36 +0200

    refs #cursor - feature

--- HW04/src/main/java/hw04/task1/Main.java
+++ HW04/src/main/java/hw04/task1/Main.java
@@ -1,73 +1,69 @@
 package hw04.task1;
 
-import hw04.task1.database.entities.User;
+import com.google.common.collect.ImmutableMap;
+import hw04.task1.entities.User;
+import hw04.task1.enums.Gender;
 import hw04.task1.messages.Messages;
 import hw04.task1.services.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 /**
- * The Main Class represents homework #1 #task1
+ * The Main Class represents homework #4 #task1
  *
  * @author YevhenKovalevskyi
  * @version 1.0
  */
 @Slf4j
-public class Main {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    
+    @Autowired
+    private UserService userService;
     
     public static void main(String[] args) {
         log.info(Messages.START_PROGRAM.getLogMessage());
         System.out.println(Messages.START_PROGRAM.getOutMessage());
-        
-        UserService userService = new UserService("mysql"); /* works */
-        //UserService userService = new UserService("pgsql"); /* works */
-        //UserService userService = new UserService("oracle"); /* works */
-        
-        // 1. Users with age less than 18
-        List<User> mysqlUsersTask1 = userService.getUsersFilteredByAgeLess(18, true);
-        printUsersList(mysqlUsersTask1, Messages.GETTING_USERS_AGE_LESS.getOutMessage());
-        
-        System.out.println("----------");
-        
-        // 2. Users with name ends in "o"
-        List<User> mysqlUsersTask2 = userService.getUsersFilteredByNameEnds("o");
-        printUsersList(mysqlUsersTask2, Messages.GETTING_USERS_NAME_ENDS.getOutMessage());
     
-        System.out.println("----------");
-        
-        // 3. Users with age between 18 and 60
-        List<User> mysqlUsersTask3 = userService.getUsersFilteredByAgeBetween(18, 60);
-        printUsersList(mysqlUsersTask3, Messages.GETTING_USERS_AGE_BETWEEN.getOutMessage());
-    
-        System.out.println("----------");
-
-        // 4. The count of Users with name contains "a"
-        int mysqlUsersTask4 = userService.getUsersCountFilteredByNameContains("a");
-        printUsersCount(mysqlUsersTask4, Messages.GETTING_USERS_COUNT_NAME_CONT.getOutMessage());
-    
-        System.out.println("----------");
-    
-        // 5. The count of Users with age more than 18
-        int mysqlUsersTask5 = userService.getUsersCountFilteredByAgeMore(18, false);
-        printUsersCount(mysqlUsersTask5, Messages.GETTING_USERS_COUNT_ADULT.getOutMessage());
+        SpringApplication.run(Main.class, args);
         
         log.info(Messages.END_PROGRAM.getLogMessage());
         System.out.println(Messages.END_PROGRAM.getOutMessage());
     }
     
-    private static void printUsersList(List<User> users, String title) {
-        System.out.printf("%s:\n", title);
+    @Override
+    public void run(String... args) {
+        /*Map<String, String> user1Params = ImmutableMap.of(
+                "email", "kcocklie0@desdev.cn", "password", "EsEYv0kix", "firstName", "Kaila", "lastName", "Cocklie",
+                "gender", Gender.FEMALE.getGender(), "age", "32"
+        );
+        Map<String, String> user2Params = ImmutableMap.of(
+                "email", "rtreversh2@zimbio.com", "password", "3CzwrESph3", "firstName", "Rufus", "lastName", "Treversh",
+                "gender", Gender.MALE.getGender(), "age", "32"
+        );
         
-        if (users.isEmpty()) {
-            System.out.println(Messages.USERS_LIST_EMPTY.getOutMessage());
-        } else {
-            users.forEach(System.out::println);
-        }
-    }
+        User user1 = User.build(user1Params, 11);
+        User user2 = User.build(user2Params);
+        
+        userService.saveUser(user1);
+        userService.createUser(user2);*/
     
-    private static void printUsersCount(int users, String title) {
-        System.out.printf("%s:\n", title);
-        System.out.printf(Messages.USERS_COUNT.getOutMessage(), users);
+        //List<User> users = userService.selectAll();
+        Optional<User> user = userService.selectOne(13);
+        
+        if (user.isPresent()) {
+            System.out.println("---");
+            System.out.println(user.get());
+            System.out.println("---");
+        } else {
+            System.out.println("---\nNot found!\n---");
+        }
     }
 }
--- HW05/src/main/java/hw05/task1/messages/Messages.java
+++ HW05/src/main/java/hw05/task1/messages/Messages.java
@@ -1,4 +1,4 @@
-package hw04.task1.messages;
+package hw05.task1.messages;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,11 +10,7 @@ import lombok.Getter;
 public enum Messages {
     START_PROGRAM("Start of the program...", "\nStart of the program...\n\n----------"),
     END_PROGRAM("End of the program.\n", "----------\n\nEnd of the program.\n"),
-    
-    USER_DELETED("Deleting user result: user deleted!", " : Deleting user result: user deleted!"),
-    USER_NOT_DELETED("Deleting user result: user not found!", " : Deleting user result: user not found!"),
-    USER_UPDATED("Updating user result: user updated!", " : Updating user result: user updated!"),
-    USER_INSERTED("Inserting user result: user inserted!", " : Inserting user result: user inserted!"),
+
     ;
     
     @Getter private final String logMessage;
