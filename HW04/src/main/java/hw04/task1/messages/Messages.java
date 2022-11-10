8ecdbb08401c489f6220ae2638be55fe3c08adc5
YevhenKovalevskyi <carsar.sd@gmail.com>
2022-11-10 21:16:28 +0200

    refs #cursor - hw06. Jpa/Hibernate fixes

--- HW04/src/main/java/hw04/task1/messages/Messages.java
+++ HW04/src/main/java/hw04/task1/messages/Messages.java
@@ -11,10 +11,17 @@ public enum Messages {
     START_PROGRAM("Start of the program...", "\nStart of the program...\n\n----------"),
     END_PROGRAM("End of the program.\n", "----------\n\nEnd of the program.\n"),
     
-    USER_DELETED("Deleting user result: user deleted!", " : Deleting user result: user deleted!"),
-    USER_NOT_DELETED("Deleting user result: user not found!", " : Deleting user result: user not found!"),
-    USER_UPDATED("Updating user result: user updated!", " : Updating user result: user updated!"),
-    USER_INSERTED("Inserting user result: user inserted!", " : Inserting user result: user inserted!"),
+    INCORRECT_CHOICE("Incorrect choose!", "-> Incorrect choice!"),
+    USER_CREATED("User id {} created!", "User id %s created!\n"),
+    USER_SAVED("User id {} saved!", "User id %s saved!\n"),
+    USER_DELETED("User id {} deleted!", "User id %s deleted!\n"),
+    USER_NOT_FOUND("User id {} not found!", "User id %s not found!\n"),
+    USERS_NOT_FOUND("Users not found!", "Users not found!"),
+    CONSOLE_OK("OK!", "OK!"),
+    CONSOLE_NOK("NOT OK! Value is incorrect!", "NOT OK! Value is incorrect!"),
+    GOT_FROM_CONSOLE("Got {} from console: [{}]. Validation: {}", "Got %s from console: [%s]. Validation: %s"),
+    CHOOSE_ACTION("-> Choose {}: ", "-> Choose %s: "),
+    ENTER_VALUE("-> Enter {}: ", "-> Enter %s: "),
     ;
     
     @Getter private final String logMessage;
