313e0db2e9832c226471452d54ed97776aa93461
YevhenKovalevskyi <carsar.sd@gmail.com>
2022-11-17 03:45:27 +0200

    refs #cursor - hw09. Hibernate Advanced

--- HW07/src/main/java/hw07/task1/messages/Messages.java
+++ HW07/src/main/java/hw07/task1/messages/Messages.java
@@ -10,7 +10,11 @@ import lombok.Getter;
 public enum Messages {
     START_PROGRAM("Start of the program...", "\nStart of the program...\n\n----------"),
     END_PROGRAM("End of the program.\n", "----------\n\nEnd of the program.\n"),
-
+    
+    TEACHER_NOT_FOUND("Teacher ID[{}] not found!", "Teacher ID[%s] not found!"),
+    GROUP_NOT_FOUND("Group ID[{}] not found!", "Group ID[%s] not found!"),
+    STUDENT_NOT_FOUND("Student ID[{}] not found!", "Student ID[%s] not found!"),
+    
     ;
     
     @Getter private final String logMessage;
